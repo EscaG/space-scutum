b9e16ceab2295cd99a61a05b5bc8932dd67ba520
Hlushko Hrygorii <escagt82@gmail.com>
2025-11-30 13:19:45 +0200

    Add GitHub Actions deploy workflow

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,20 +1,8 @@
-import {Route, Routes} from "react-router-dom";
-import {HomePage} from "@pages/Home/HomePage.tsx";
-import {NotFoundPage} from "@pages/NotFound/NotFound.tsx";
-import {MainLayout} from "@app/layout/MainLayout.tsx";
-import {ToDoPage} from "@pages/ToDo/ToDoPage.tsx";
+import {AppRouter} from "@app/providers/appRouter/AppRouter.tsx";
 
 export default function App() {
 
 	return (
-		<Routes>
-			<Route element={<MainLayout/>}>
-				<Route path="/" element={<HomePage/>}/>
-				<Route path="/todo/:id" element={<ToDoPage />} />
-			</Route>
-
-			{/*A separate 404 page to display without a header*/}
-			<Route path="*" element={<NotFoundPage/>}/>
-		</Routes>
+		<AppRouter/>
 	);
 }
--- src/app/providers/appRouter/AppRouter.tsx
+++ src/app/providers/appRouter/AppRouter.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import {Routes, Route} from 'react-router-dom';
 import {NotFoundPage} from "@pages/NotFound/NotFound.tsx";
 import {HomePage} from "@pages/Home/HomePage.tsx";
+import {MainLayout} from "@app/layout/MainLayout.tsx";
+import {ToDoPage} from "@pages/ToDo/ToDoPage.tsx";
 
 
 export const AppRouter: React.FC = () => {
 	return (
 		<Routes>
-			<Route path='/' element={<HomePage />} />
-			<Route path='*' element={<NotFoundPage />} />
+			<Route element={<MainLayout/>}>
+				<Route path="/" element={<HomePage/>}/>
+				<Route path="/todo/:id" element={<ToDoPage/>}/>
+			</Route>
+
+			{/*A separate 404 page to display without a header*/}
+			<Route path="*" element={<NotFoundPage/>}/>
 		</Routes>
 	);
 };
